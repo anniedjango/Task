f496338bdbf086591caa4c5b03aabdc76dd34fe0
ANNIE SL <annieskdas2000@gmail.com>
2023-04-24 08:35:26 -0700

    add

---
+++ mockproject/credentials/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+
+    path('register',views.register,name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('home', views.home, name='home'),
+    path('but', views.but, name='but'),
+    path('form', views.form, name='form'),
+    path('acp', views.acp, name='acp'),
+
+
+
+
+]
